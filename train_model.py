36aafd1ccec3437939101a384dc25677aadb1573
Jes <jesinta05@gmail.com>
2024-08-03 16:33:42 +0530

    updating filepaths

--- train_model.py
+++ train_model.py
@@ -6,9 +6,9 @@ import data_preprocessing
 # Load processed data
 X = data_preprocessing.X
 y = data_preprocessing.y
-file_path = 'D:/Mtech/Semester3/MLOPS/Assignment/mlops_assignment_1_group_56/train_output'
+file_path = '/home/runner/work/mlops_assignment_1_group_56/train_output'
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
-output_file_path = 'D:/Mtech/Semester3/MLOPS/Assignment/mlops_assignment_1_group_56/test_output'
+output_file_path = '/home/runner/work/mlops_assignment_1_group_56/test_output'
 # Train model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
@@ -21,4 +21,4 @@ X_test.to_csv(output_file_path+'/X_test.csv')
 y_test.to_csv(output_file_path+'/Y_test.csv')
 
 # Save the model
-dump(model, 'D:/Mtech/Semester3/MLOPS/Assignment/mlops_assignment_1_group_56/models/model.joblib')
+dump(model, '/home/runner/work/mlops_assignment_1_group_56/models/model.joblib')
